88d4944b4db1546065090dfb923fc1bf7b1741eb
Roman Cristian <romancristianroman@yahoo.com>
2022-07-06 13:49:29 +0300

    [FEATURE] a function helping at transforming a matrix stored as a string into one storing data as integers

---
+++ MagicSquare/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace ExtensionMethods;
+
+public static class StringExtensions 
+{
+    public static int GetNumberAtPosition(this string matrix, int startPosition)
+    {
+        if(IsNumber(matrix[startPosition]))
+        {
+            int number = 0;
+            int i = startPosition;
+            while (i < matrix.Length && IsNumber(matrix[i]))
+            {
+                int digit = (int)(matrix[i] - '0');
+                number = number * 10 + digit;
+                i++;
+            }
+
+            return number;
+        }
+        else
+        {
+            throw new Exception("Can't extract a number because starting position doesn't match a number.");
+        }
+    }
+    
+    private static bool IsNumber(char c)
+    {
+        if (c >= '0' && c <= '9')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
